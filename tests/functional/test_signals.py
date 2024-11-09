9a113fed6bd245f602a9a636c9f9f86a86157bd3
Rok Mandeljc <rok.mandeljc@gmail.com>
2024-11-09 13:46:59 +0100

    tests: increase the waiting time in test_onefile_signal_handling
    Increase the waiting time (the timeout) when running the
    `test_onefile_signal_handling` in signal-forwarding mode from
    1 second to 5 seconds. This should take care of sporadic test
    failures when running under CPU contention (number of pytest
    runners matching or exceeding the number of available CPUs).
    
    The waiting time for signal-ignoring mode is left at 1 second,
    to avoid unnecessarily slowing down the test (since in contrast
    to signal-forwarding test, signal-ignoring one needs to wait for
    the whole timeout interval).

--- tests/functional/test_signals.py
+++ tests/functional/test_signals.py
@@ -33,13 +33,16 @@ def test_onefile_signal_handling(pyi_builder, forward_signals):
 
         # Quietly exit if no signal number is given on command-line. This accommodates the fact that
         # `pyi_builder.test_source()` always runs the program after building it.
-        if len(sys.argv) < 2:
-            print(f"Usage: {sys.argv[0]} <signal-number>", file=sys.stderr)
+        if len(sys.argv) < 3:
+            print(f"Usage: {sys.argv[0]} <signal-number> <timeout>", file=sys.stderr)
             sys.exit(0)
 
-        # Signal number is passed as first command-line argument
+        # Signal number is passed as the first command-line argument
         signal_number = int(sys.argv[1])
         signal_name = signal.Signals(signal_number).name  # This implicitly validates signal number
+
+        # Timeout is passed as the second command-line argument
+        timeout = float(sys.argv[2])
 
         # Return code: received signal number, or zero if signal was not received.
         return_code = 0
@@ -60,14 +63,16 @@ def test_onefile_signal_handling(pyi_builder, forward_signals):
         )
         os.kill(parent_pid, signal_number)
 
-        # Wait up to a second for signal to be delivered.
-        for i in range(10):
+        # Wait for signal to be delivered or the specified timeout interval to pass.
+        start_time = time.time()
+        while True:
+            if time.time() - start_time >= timeout:
+                print(f"Signal not received within {timeout} seconds!", file=sys.stderr)
+                break
             if return_code != 0:
                 print("Signal received!", file=sys.stderr)
                 break
-            time.sleep(0.1)
-        else:
-            print("Signal not received!", file=sys.stderr)
+            time.sleep(0.1)  # 100 ms steps
 
         sys.exit(return_code)
         """,
@@ -100,9 +105,19 @@ def test_onefile_signal_handling(pyi_builder, forward_signals):
         # Expected return code: signal number in signal-forwarding mode, zero otherwise.
         expected_code = signal_number if forward_signals else 0
 
+        # Timeout interval when waiting for signal to be delivered (or not): in signal-forwarding mode, wait up to 5
+        # seconds, to be on the safe side and avoid sporadic test failures. The initial 1-second interval is sometimes
+        # too short when running tests under CPU contention scenario (number of pytest runners matching or exceeding
+        # number of CPU cores). Since the test program exits as soon as signal is received, we expect to never hit this
+        # 5-seconds limit. In contrast, when in signal-ignoring mode, we need to wait for the whole interval to see that
+        # the signal is not delivered. In this case, we stick with original 1-second timeout to avoid unnecessarily
+        # slowing down the test; as mentioned earlier, we expect signals to be delivered within 1 second for most of the
+        # time, so if ignore mode was broken, at least some tests would fail.
+        timeout = 5 if forward_signals else 1  # seconds
+
         # Run
         print(f"=== running test with {signal_name} ===", file=sys.stderr)
-        status = subprocess.run([program_exe, str(signal_number)])
+        status = subprocess.run([program_exe, str(signal_number), str(timeout)])
         ret_code = status.returncode
 
         print(f"=== program returned code {ret_code} (expected {expected_code}) ===", file=sys.stderr)
